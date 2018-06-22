fc540c799e822d2629cdd26ea47b26d94ed7c69b
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-06-22 21:10:03 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,18 +1,4 @@
 class Owner
-def initialize(name)
-    @name = name
-    @mood = "nervous"
-  end
-  
-  def name
-    return @name
-  end
-  
-  def mood
-    return @mood
-  end
-  
-  def mood=(mood)
-    @mood = mood
-  end
+def initialize(species)
+  @species = species
 end
