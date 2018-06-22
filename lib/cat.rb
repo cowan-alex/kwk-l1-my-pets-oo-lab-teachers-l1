ca5311575a23e2a8a66fdfceab43c17344e67784
Alex Cowan <alexandria.cowan.17@tfacorps.org>
2018-06-22 22:15:48 +0000

    Done.

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  def initialize(name)
+    @name = name
+    @mood = "nervous"
+  end
+  
+  def name
+    return @name
+  end
+  
+  def mood
+    return @mood
+  end
+  
+  def mood=(mood)
+    @mood = mood
+  end
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,4 +1,43 @@
 class Owner
-  # code goes here
-
- end
+  def initialize(species)
+    @species = species
+    @pets = {:fishes => [], :dogs => [], :cats => []}
+  end
+  
+  def species
+    return @species
+  end
+  
+  def say_species
+    return "I am a #{species}."
+  end
+  
+  def name
+    return @name
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  def pets
+    return @pets
+  end
+  
+  
+  def buy_fish(buy_fish)
+    @pets[:fishes] << buy_fish
+    end
+  
+  def buy_cat(buy_cat)
+    @pets[:cats] << buy_cat
+  end
+  
+  def buy_dog(buy_dog)
+    @pets[:dogs] << buy_dog
+  end
+  
+  def list_pets
+    return "I have #{@pets[:fishes].size} fish, #{@pets[:dogs].size} dog(s), and #{@pets[:cats].size} cat(s)."
+  end
+end
